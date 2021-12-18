d879043be19a462f9ab262892feaf4acd8c6541d
Andreas <77636627+andr8020@users.noreply.github.com>
2021-12-18 16:43:46 +0100

    mere static debug
    title

--- DjangoProjekt/config/config/settings.py
+++ DjangoProjekt/config/config/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type

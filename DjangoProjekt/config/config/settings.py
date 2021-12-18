1119de6b633b2b60ab2a3cd776c131dc28cd0d70
Andreas <77636627+andr8020@users.noreply.github.com>
2021-12-18 19:36:15 +0100

    static

--- DjangoProjekt/config/config/settings.py
+++ DjangoProjekt/config/config/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -144,7 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'templates/static')
 ]
 
 # Default primary key field type
